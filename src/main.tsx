732ab1789c801bb4b9affe718b3e39db4e2ef8ac
Fernando Ticona <46249834+thefersh@users.noreply.github.com>
2022-09-20 13:36:22 +0000

    loadding app

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root") as HTMLElement;
+  root.classList.add("netflix-logo-out");
+  setTimeout(() => {
+    if (root !== null) {
+      ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      );
+    }
+  }, 1000);
+});
