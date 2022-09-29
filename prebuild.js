2b63056b49c3bbbcdef89bf139bc8e82ec767c57
Fernando Ticona <thefersh24@gmail.com>
2022-09-28 21:35:16 -0400

    fix: __dirname

--- prebuild.js
+++ prebuild.js
@@ -2,5 +2,5 @@ import { existsSync, writeFileSync } from 'fs'
 import { join } from 'path'
 
 if(!existsSync('.env')) {
-  writeFileSync(join(__dirname, 'env'), `VITE_TMDB_API_KEY=${process.env.VITE_TMDB_API_KEY}`)
+  writeFileSync(join(process.cwd(), '.env'), `VITE_TMDB_API_KEY=${process.env.VITE_TMDB_API_KEY}`)
 }
